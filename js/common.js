2ecb19776bb931ea0819ced0c7b718ef9d2e53e0
moudoder <39887984+moudoder@users.noreply.github.com>
2021-08-09 15:20:10 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -8,4 +8,48 @@ $(document).ready(function () {
     prevArrow: $('.section-arrow__left'),
     nextArrow: $('.section-arrow__right'),
   });
+  $('.trust-slider').slick({
+    infinite: true,
+    variableWidth: true,
+    swipe: true,
+    centerMode: true,
+    slidesToShow: 1,
+    prevArrow: $('.section-arrow__left_t'),
+    nextArrow: $('.section-arrow__right_t'),
+  });
+  $('.transportation-row').slick({
+    infinite: true,
+    variableWidth: true,
+    swipe: true,
+    dots: true,
+    slidesToShow: 1,
+    prevArrow: $('.section-arrow__left_c'),
+    nextArrow: $('.section-arrow__right_c'),
+    mobileFirst: true,
+     responsive: [
+        {
+           breakpoint: 1000,
+           settings: "unslick"
+        }
+     ]
+  });
+  $('.cases-slider').slick({
+    infinite: true,
+    variableWidth: true,
+    swipe: true,
+    dots: true,
+    slidesToShow: 1,
+    prevArrow: $('.section-arrow__left_c'),
+    nextArrow: $('.section-arrow__right_c'),
+  });
+  $('.questions-block').on('click', function() {
+    $('.questions-block').removeClass('questions-block-active');
+    $(this).addClass('questions-block-active');
+  })
+  $(".nav-list__link").on("click", function (event) {
+    event.preventDefault();
+    let id  = $(this).attr('href'),
+    top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 1500);
+  });
 })
