a81d72b38a1755b7c057272a6699e4d1760abeaa
moudoder <39887984+moudoder@users.noreply.github.com>
2021-08-13 15:07:09 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,5 +1,50 @@
 $(document).ready(function () {
   new WOW().init();
+  $('.btn-call').on('click', function() {
+    $('.modal-window').addClass('modal-window-active');
+    $('.dark-window').addClass('dark-window-active');
+    $('.mobile-menu').removeClass('mobile-menu-active');
+    return false;
+  })
+  $('.body-menu-item__form').on('click', function() {
+    $('.job-block-modal').addClass('job-block-modal-active');
+    $('.dark-window').addClass('dark-window-active');
+    return false;
+  })
+  $('.job-way-block').on('click', function() {
+    $('.job-way-block').removeClass('job-way-block-active');
+    $(this).addClass('job-way-block-active');
+  })
+  $('.contacts-block').on('click', function() {
+    $('.contacts-block').removeClass('contacts-block-active');
+    $(this).addClass('contacts-block-active');
+  })
+  $('.contacts-right-wrapper').slick({
+    infinite: true,
+    swipe: true,
+    slidesToShow: 1,
+  });
+  $('.contacts-block-1').on('click', function() {
+    $('.contacts-right-wrapper').slick('slickGoTo', 0);
+  })
+  $('.contacts-block-2').on('click', function() {
+    $('.contacts-right-wrapper').slick('slickGoTo', 1);
+  })
+  $('.contacts-block-3').on('click', function() {
+    $('.contacts-right-wrapper').slick('slickGoTo', 2);
+  })
+  $('.dark-window').on('click', function() {
+    $('.modal-window').removeClass('modal-window-active');
+    $('.job-block-modal').removeClass('job-block-modal-active');
+    $('.dark-window').removeClass('dark-window-active');
+    return false;
+  })
+  $('.modal-window__close').on('click', function() {
+    $('.job-block-modal').removeClass('job-block-modal-active');
+    $('.modal-window').removeClass('modal-window-active');
+    $('.dark-window').removeClass('dark-window-active');
+    return false;
+  })
   $('.auto-row').slick({
     infinite: true,
     variableWidth: true,
@@ -95,7 +140,7 @@ $(document).ready(function () {
     return false;
   })
   let $element = $('#auto');
-  let nav = $('.header-nav');
+  let nav = $('.nav-abc');
   $(window).scroll(function() {
     let scroll = $(window).scrollTop() + $(window).height();
     //Если скролл до конца елемента
@@ -104,10 +149,10 @@ $(document).ready(function () {
     let offset = $element.offset().top
    
     if (scroll > offset) {
-      $(nav).addClass('nav-fixed');
+      $(nav).addClass('nav-abc-active');
     }
     else{
-      $(nav).removeClass('nav-fixed');
+      $(nav).removeClass('nav-abc-active');
     }
   });
 
